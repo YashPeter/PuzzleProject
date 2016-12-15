16fcc2a25e12deaf52eb875d0a7581381dcc13ee
Peter and Yash <peterkosowan@hotmail.com>
2016-12-15 15:18:46 -0700

    Alot 2
    ALot

---
+++ PuzzleProject/Assets/MainWorld.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//Player test1 = new Player (0,0,3);
+		PlayerDisplay t1 = 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ PuzzleProject/Assets/Player/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player{
+	float posx;
+	float posy;
+	int direction;
+	int frame;
+	int framelimit;
+	public Player(float posx, float posy, int framelimit){
+		this.posx = posx;
+		this.posy = posy;
+		direction = 0;
+		frame = 0;
+		this.framelimit = frame;
+	}
+	public float getposx(){
+		return posx;
+	}
+
+	public float getposy(){
+		return posy;
+	}
+
+	public float getdirection(){
+		return direction;
+	}
+
+	public float getframe(){
+		return frame;
+	}
+
+	public void move(){
+		switch (direction) {
+		case 0:
+			posy -= 1;
+			updateframe ();
+		break;	
+		case 1:
+			posx += 1;
+			updateframe ();
+			break;	
+		case 2:
+			posy += 1;
+			updateframe ();
+			break;	
+		case 3:
+			posx -= 1;
+			updateframe ();
+			break;	
+		}
+	}
+
+	public void updateframe(){
+		if (frame == framelimit) {
+			frame = 0;
+		} else {
+			frame += 1;
+		}
+	}
+
+	public void setdirection(int direction){
+		this.direction = direction;
+	}
+}
